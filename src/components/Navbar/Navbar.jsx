3af30648d464f5edf525cf2cdccadf3947df1aab
SriCoD <72248660+SriCoD@users.noreply.github.com>
2023-12-27 23:51:24 +0530

    Components added

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="left"></div>
+      <div className="wrapper"></div>
+      <div className="right"></div>
+      <div className="centre"></div>
+    </div>
+  );
+};
+
+export default Navbar;
