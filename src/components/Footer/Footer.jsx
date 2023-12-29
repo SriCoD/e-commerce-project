f212011af9e581f41b04e7816ba3fd78f77c6c99
SriCoD <72248660+SriCoD@users.noreply.github.com>
2023-12-29 18:23:23 +0530

    Footer Section Completed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,50 @@
 import React from "react";
-
+import "./Footer.scss";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="top">
+        <div className="item">
+          <h1>Categories</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Shoes</span>
+          <span>Acessories</span>
+          <span>New Arrivals</span>
+        </div>
+        <div className="item">
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Cookies</span>
+          <span>Compare</span>
+        </div>
+        <div className="item">
+          <h1>About</h1>
+          <span>lorem</span>
+        </div>
+        <div className="item">
+          <h1>Contact Us</h1>
+          <span>
+            <a href="mailto:srishtibkma@gmail.com">Email</a>
+          </span>
+          <span>
+            <a href="tel:9862516905">Phone</a>
+          </span>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <span className="logo">Srishti Store</span>
+          <span className="copyright">Copyrights 2024.All rights reserved</span>
+        </div>
+        <div className="right ">
+          <img src="/img/payment.jpg" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
