b21f52f0af589e9cfdd0c730ea6c807bb0fa35f7
SriCoD <72248660+SriCoD@users.noreply.github.com>
2024-01-01 14:54:07 +0530

    Categories Page Completed

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,48 +1,89 @@
 import React from "react";
 import "./Categories.scss";
 import { Link } from "react-router-dom";
+
 const Categories = () => {
   return (
     <div className="categories">
       <div className="col">
         <div className="row">
           <img
-            src="https://images.pexels.com/photos/2085118/pexels-photo-2085118.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt="img"
+            src="https://images.pexels.com/photos/818992/pexels-photo-818992.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt=""
           />
           <button>
-            <Link to="/products/1" className="link">
+            <Link className="link" to="/products/1">
               Sale
             </Link>
           </button>
         </div>
         <div className="row">
-          <img src="https://images.pexels.com/photos/1201613/pexels-photo-1201613.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+          <img
+            src="https://images.pexels.com/photos/2036646/pexels-photo-2036646.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt=""
+          />
+          <button>
+            <Link to="/products/1" className="link">
+              Women
+            </Link>
+          </button>
         </div>
       </div>
       <div className="col">
         <div className="row">
-          <img src="https://images.pexels.com/photos/1126993/pexels-photo-1126993.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+          {" "}
+          <img
+            src="https://images.pexels.com/photos/1813947/pexels-photo-1813947.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt=""
+          />
+          <button>
+            <Link to="/products/1" className="link">
+              New Season
+            </Link>
+          </button>
         </div>
       </div>
-      <div className="col col-1"></div>
-      <div className="row">
-        <div className="col">
-          <div className="row">
-            <img
-              src="
-https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            />
+      <div className="col col-l">
+        <div className="row">
+          <div className="col">
+            <div className="row">
+              <img
+                src="https://images.pexels.com/photos/1192609/pexels-photo-1192609.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <button>
+                <Link to="/products/1" className="link">
+                  Men
+                </Link>
+              </button>
+            </div>
+          </div>
+          <div className="col">
+            <div className="row">
+              {" "}
+              <img
+                src="https://images.pexels.com/photos/2703202/pexels-photo-2703202.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <button>
+                <Link to="/products/1" className="link">
+                  Accessories
+                </Link>
+              </button>
+            </div>
           </div>
         </div>
-        <div className="col">
-          <div className="row">
-            <img src="https://images.pexels.com/photos/1034859/pexels-photo-1034859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-          </div>
+        <div className="row">
+          <img
+            src="https://images.pexels.com/photos/1159670/pexels-photo-1159670.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt=""
+          />
+          <button>
+            <Link to="/products/1" className="link">
+              Shoes
+            </Link>
+          </button>
         </div>
-      </div>
-      <div className="row">
-        <img src="https://images.pexels.com/photos/932417/pexels-photo-932417.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
       </div>
     </div>
   );
